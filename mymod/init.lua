4e460b5fdebf9969e076e1d9e5bec69f2dd659ef
edu1010 <39130123+edu1010@users.noreply.github.com>
2020-11-23 20:59:25 +0100

    ERROR CORCHETES no encuentro cual esta mal

--- mymod/init.lua
+++ mymod/init.lua
@@ -16,6 +16,34 @@ minetest.register_craftitem("mymod:ingotOri", {
   inventory_image = "oriIngot.png"
 })
 
+
+
+
+minetest.register_tool("default:swordOri", {
+    description = "Steel ori",
+    inventory_image = "default_tool_steelsword.png",
+    tool_capabilities = {
+        -- Digging capabilities
+        max_drop_level = 1,
+        groupcaps = {
+            fleshy={times={[1]=2.00, [2]=0.80, [3]=0.40}, uses=10, maxlevel=2},
+            snappy={times={[2]=0.70, [3]=0.30}, uses=40, maxlevel=1},
+            choppy={times={[3]=0.65}, uses=40, maxlevel=0}
+        },
+        -- Damage capabilities
+        full_punch_interval = 0.8,
+        damage_groups = {fleshy=8, snappy=4, choppy=2},
+    }
+})
+
+minetest.register_craft({
+      output = "mymod:swordOri 1"
+      recipe = {
+        {"mymod:ingotOri","mymod:ingotOri",},
+        {"mymod:ingotOri","mymod:ingotOri",},
+      }
+})
+--[[
 minetest.register_craft({
   type = "cooking",
   output = "mymod:ingotOri",
@@ -33,5 +61,6 @@ minetest.register_craft({
   output = "mymod:pickOri",
   recipe = ""
 })
+--]]
 
 
