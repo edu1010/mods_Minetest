67366f18a4ad32b392a3d979c6944ede0c588fcc
Ros <72873067+paularosm@users.noreply.github.com>
2020-11-23 15:52:47 +0100

    ori: en todas partes

--- Ori/init.lua
+++ Ori/init.lua
@@ -1,11 +1,11 @@
-minetest.register_node("mymod:orichalcum", {
+minetest.register_node("ori:orichalcum", {
     description = "Alien orichalcum",
     tiles = {"mymod_diamond.png"},
     is_ground_content = true,
     groups = {cracky=3, stone=1}
 })
 
-minetest.register_node("mymod:orichalcum", {
+minetest.register_node("ori:orichalcum", {
     description = "Alien orichalcum",
     tiles = {
         "mymod_diamond_up.png",    -- y+
@@ -17,28 +17,28 @@ minetest.register_node("mymod:orichalcum", {
     },
     is_ground_content = true,
     groups = {cracky = 3},
-    drop = "mymod:diamond_fragments"
+    drop = "ori:diamond_fragments"
     -- ^  Rather than dropping orichalcum, drop mymod:diamond_fragments
 })
 
 -- craftitem lump
-minetest.register_craftitem("mymod:orichalcum_lump", {
+minetest.register_craftitem("ori:orichalcum_lump", {
 description = "Orichalcum Lump",
-inventory_image = "mymod_myitem.png"
+inventory_image = "ori_myitem.png"
 })
 -- craftitem ingot
-minetest.register_craftitem("mymod:orichalcum_ingot", {
+minetest.register_craftitem("ori:orichalcum_ingot", {
 description = "Orichalcum Ingot",
 inventory_image = "mymod_myitem.png"
 })
 -- craft de ingot
 minetest.register_craft({
     type = "cooking",
-    output = "mymod:orichalcum_ingot",
+    output = "ori:orichalcum_ingot",
     recipe = {
-        {"mymod:orichalcum_lump", "",  ""},
-        {"mymod:orichalcum_lump", "",  ""},
-        {"mymod:orichalcum_lump", "",  ""}
+        {"ori:orichalcum_lump", "",  ""},
+        {"ori:orichalcum_lump", "",  ""},
+        {"ori:orichalcum_lump", "",  ""}
     }
     cooktime = 10
 })
