2e00d347525bf37feed5d0c169c4decba9a4f3e7
edu1010 <39130123+edu1010@users.noreply.github.com>
2020-11-23 11:34:41 +0100

    comit inical

---
+++ mods/mymod/init.lua
@@ -0,0 +1,10 @@
+minetest.register_node("mymod:mynode", {
+description = "My node",
+tiles = {"mymod_mynode.png"},
+groups = {cracky=3}
+})
+minetest.register_craftitem("mymod:myitem", {
+description = "My Item",
+inventory_image = "mymod_myitem.png"
+})
+--/grantme all
