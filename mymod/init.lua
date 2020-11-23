7eb0baf0696a3e64f92a43e1760246ca64280b11
Ros <72873067+paularosm@users.noreply.github.com>
2020-11-23 19:50:35 +0100

    ori pngs y ej de register_tool

--- mymod/init.lua
+++ mymod/init.lua
@@ -1,12 +1,37 @@
 minetest.register_craftitem("mymod:stoneOri", {
-    description = "stone with orichalcum",
-    inventory_image = "oriStone.png"
+  description = "stone with orichalcum",
+  inventory_image = "oriStone.png"
 })
 
+minetest.register_node("mymod:nodeOri", {
+  description = "orichalcum node",
+  tiles = {"oriStone.png"},
+  is_ground_content = true,
+  groups = {cracky = 3},
+  drop = "mymod:stoneOri"
+})
+
+minetest.register_craftitem("mymod:ingotOri", {
+  description = "ori's ingot",
+  inventory_image = "oriIngot.png"
+})
 
 minetest.register_craft({
-    type = "cooking",
-    output = "mymod:stoneOri_fragments",
-    recipe = "default:coalblock",
-    cooktime = 10,
+  type = "cooking",
+  output = "mymod:ingotOri",
+  recipe = "mymod:stoneOri",
+  cooktime = 4
 })
+
+-- set completo de herramientas:
+minetest.register_tool("mymod:pickOri", {
+  
+})
+
+minetest.register_craft({
+  type = "shapeless", --? ns
+  output = "mymod:pickOri",
+  recipe = ""
+})
+
+
