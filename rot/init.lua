d7fd3d04bf8eb8a550b8a3a384d40b485d5e171e
edu1010 <39130123+edu1010@users.noreply.github.com>
2020-11-24 20:51:10 +0100

    primer abm

--- rot/init.lua
+++ rot/init.lua
@@ -10,3 +10,16 @@ minetest.register_node("rot:purgator", {
   is_ground_content = true,
   groups = {cracky = 3},
 })
+
+
+minetest.register_abm({
+ nodenames = {"default:dirt_with_grass"},
+ neighbors = {"default:dirt_with_grass"},-- to avoid the spawn of same rot_dirt next each other
+ interval = 30.0,
+ chance = 350,
+ catch_up = false,
+ action = function(pos, node, active_object_count, active_object_count_wider)
+ local pos = {x = pos.x, y = pos.y, z = pos.z}
+ minetest.set_node(pos, {name = "rot:dirt"})
+ end
+})
