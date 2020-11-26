abef86e3cb02325c006483f00833e1333c62de03
Ros <72873067+paularosm@users.noreply.github.com>
2020-11-26 19:39:18 +0100

    Update init.lua

--- teleport/init.lua
+++ teleport/init.lua
@@ -12,19 +12,28 @@ minetest.register_node("teleport:pad", {
   },
   is_ground_content = true,
   groups = {cracky = 3},
-  on_punch = function(pos, node, player, pointed_thing)
+  after_place_node = function(pos, node, player, pointed_thing)
     local player = minetest.get_player_by_name("singleplayer")
     local inv = player:get_inventory()
     local stack = ItemStack("teleport:command 1")
     local leftover = inv:add_item("main", stack)
+    
+    local meta = stack:get_meta()
+    meta:set_int("x", pos.x)
+    meta:set_int("y", pos.y)
+    meta:set_int("z", pos.z)
+
   end,
-  --[[local meta = stack:get_meta()
-  meta:set_int("x", pos.x)
-  meta:set_int("y", pos.y)
-  meta:set_int("z", pos.z)  --]]
+
 })
 minetest.register_craftitem("teleport:command", {
   description = "object to be teleported to the platform",
   inventory_image = "mando.png",
+  
+  on_use = function(pos, node, player, pointed_thing)
+    local meta = itemstack:get_meta() --itemstack??
+    local pos = {x=meta:get_int("x"), y=meta:get_int("y"), z=meta:get_int("z")}
+    player:set_pos(pos)
+  end,
 })
 
