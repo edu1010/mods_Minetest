ca0a2280b057364046a0d8a358c74a9a16a469e8
Ros <72873067+paularosm@users.noreply.github.com>
2020-11-28 17:50:24 +0100

    Update init.lua

--- teleport/init.lua
+++ teleport/init.lua
@@ -1,5 +1,3 @@
---default_stone_brick.png
-
 minetest.register_node("teleport:pad", {
   description = "this node is a teleport platform",
   tiles = {
@@ -12,13 +10,12 @@ minetest.register_node("teleport:pad", {
   },
   is_ground_content = true,
   groups = {cracky = 3},
-  after_place_node = function(pos, node, player, pointed_thing)
+  after_place_node = function(pos, placer, itemstack, pointed_thing)
     local player = minetest.get_player_by_name("singleplayer")
     local inv = player:get_inventory()
-    local stack = ItemStack("teleport:command")
+    stack = ItemStack("teleport:command")
     local leftover = inv:add_item("main", stack)
-    
-    local meta = player:get_meta()
+    local meta = stack:get_meta()
     meta:set_int("x", pos.x)
     meta:set_int("y", pos.y+1)
     meta:set_int("z", pos.z)
@@ -29,9 +26,9 @@ minetest.register_craftitem("teleport:command", {
   description = "command to be teleported to the pad",
   inventory_image = "mando.png",
   
-  on_use = function(pos, node, player, pointed_thing)
+  on_use = function(itemstack, dropper, pos)
     local player = minetest.get_player_by_name("singleplayer")
-    local meta = player:get_meta()
+    local meta = stack:get_meta()
     local pos = {x=meta:get_int("x"), y=meta:get_int("y"), z=meta:get_int("z")}
     player:set_pos(pos)
   end,
